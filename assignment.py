1e6f97b1e31e16e2366de312481444736915c82e
pohhoe <51848179+pohhoe@users.noreply.github.com>
2025-01-12 11:44:50 +0800

    Assignment 1.1 Submission

--- assignment.py
+++ assignment.py
@@ -4,8 +4,8 @@
 # and "FizzBuzz" when the number is divisible by both 3 and 5.
 # If the number is not divisible by either 3 or 5, the function should return the number itself.
 
-
 def fizz_buzz(number):
+
     """Returns Fizz if number is divisible by 3, Buzz if divisible by 5, FizzBuzz if divisible by both 3 and 5.
     If not divisible by either 3 or 5, returns the number itself.
     >>> fizz_buzz(3)
@@ -15,55 +15,78 @@ def fizz_buzz(number):
     >>> fizz_buzz(15)
     'FizzBuzz'
     """
-    return
-
+    
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return number
 
 # Question 2
 
 # Write a function that takes a list of numbers and returns the sum of the squares of all the numbers.
 
-
 def sum_of_squares(numbers):
+
     """Returns the sum of the squares of all the numbers in a list.
     >>> sum_of_squares([1, 2, 3])
     14
     >>> sum_of_squares([2, 4, 6])
     56
     """
-    return
 
+    return sum(x**2 for x in numbers)
 
 # Question 3
 
 # Write a function that counts the number of vowels in a string.
 
-
 def count_vowels(string):
+
     """Returns the number of vowels in a string.
     >>> count_vowels("hello")
     2
     >>> count_vowels("aeiou")
     5
     """
-    return
 
+    vowels = "aeiouAEIOU"
+    return sum(1 for char in string if char in vowels)
 
 # Question 4
 
 # Write a function that counts the number of repeated characters in a string.
 
-
 def count_repeats(string):
+
     """Returns the number of repeated characters in a string.
     >>> count_repeats("hello")
     2
     >>> count_repeats("aeiou")
     0
     """
-    return
+    char_count = {}
+    repeated_count = 0
+    
+    # Count the occurrences of each character
+    for char in string:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    
+    # Count how many characters have more than one occurrence
+    for count in char_count.values():
+        if count > 1:
+            repeated_count += 1
 
+    return repeated_count
 
 if __name__ == "__main__":
     import doctest
-
+    
     doctest.testmod(verbose=True)
+
